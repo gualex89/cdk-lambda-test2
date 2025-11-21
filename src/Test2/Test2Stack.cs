d252893de7ae879390d3afec4e632c2143d0f59b
Gustavo <gualex89@gmail.com>
2025-11-21 09:16:32 -0300

    Initial commit

---
+++ src/Test2/Test2Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Test2
+{
+    public class Test2Stack : Stack
+    {
+        internal Test2Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
